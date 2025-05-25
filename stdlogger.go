dd82cedf5b8fa22e19a72d03448da9c4358f8277
DLC <shabrov23.02@gmail.com>
2025-05-25 17:35:46 +0300

    feat: add logger

---
+++ stdlogger.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+)
+
+type StdLogger struct{}
+
+func NewStdLogger() Logger {
+	return &StdLogger{}
+}
+
+func (s *StdLogger) Info(msg string, args ...any) {
+	log.Printf("[INFO] "+msg, args...)
+}
+
+func (s *StdLogger) Error(msg string, args ...any) {
+	log.Printf("[ERROR] "+msg, args...)
+}
+
+func (s *StdLogger) Debug(msg string, args ...any) {
+	log.Printf("[DEBUG] "+msg, args...)
+}
+
+func (s *StdLogger) Warn(msg string, args ...any) {
+	log.Printf("[WARN] "+msg, args...)
+}
+
+func (s *StdLogger) Fatal(msg string, args ...any) {
+	log.Fatalf("[FATAL] "+msg, args...)
+}
---
+++ zerologger.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+type ZeroLogger struct {
+	z zerolog.Logger
+}
+
+func NewZeroLogger(pretty bool) Logger {
+	var zl zerolog.Logger
+	if pretty {
+		zl = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
+	} else {
+		zl = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	}
+
+	return &ZeroLogger{z: zl}
+}
+
+func (z *ZeroLogger) Info(msg string, args ...any) {
+	z.z.Info().Msgf(msg, args...)
+}
+
+func (z *ZeroLogger) Error(msg string, args ...any) {
+	z.z.Error().Msgf(msg, args...)
+}
+
+func (z *ZeroLogger) Debug(msg string, args ...any) {
+	z.z.Debug().Msgf(msg, args...)
+}
+
+func (z *ZeroLogger) Warn(msg string, args ...any) {
+	z.z.Warn().Msgf(msg, args...)
+}
+
+func (z *ZeroLogger) Fatal(msg string, args ...any) {
+	z.z.Fatal().Msgf(msg, args...)
+
+}
